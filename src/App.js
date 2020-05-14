442bb4896888dad26e5581ea4dcbddddb0461f25
Pravin Anandrao Bagade <pravin.bagade@mindtree.com>
2020-05-14 19:24:58 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import CustomButton from '../node_modules/@pravin_008/custom_comp/dist/custom-button';
+//import CustomButton from '@pravin_008/custom_comp';
+
+const App = () => {
+  return (<div>
+    <div>Hello Test</div>
+    <CustomButton label={'Submit'} />
+  </div>);
+}
+
+export default App;
